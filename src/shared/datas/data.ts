052d53c95ebc5a0244c9514c1f628d623c903754
SilentCoder-ssh <azzedinehakaoui@gmail.com>
2025-01-25 08:17:53 +0100

    Created findData function with the button click event handler.

--- src/shared/datas/data.ts
+++ src/shared/datas/data.ts
@@ -1,5 +1,34 @@
-const REQUEST_DATA = await fetch(
-  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
-);
+import API_KEY from "../../hide/config";
 
-console.log(REQUEST_DATA)
+const locationInput: HTMLInputElement = document.querySelector(
+  "#location-input"
+)! as HTMLInputElement;
+const searchButton: HTMLButtonElement = document.querySelector(
+  "#search-button"
+)! as HTMLButtonElement;
+
+const apiKey = API_KEY;
+
+async function findData(cityName: string): Promise<any> {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}`;
+ 
+  try {
+    const response: Response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP: ${response.status}`);
+    };
+    
+    const data = await response.json()
+    console.log("Données reçues : ", data);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données : ", error);
+    throw error
+  }
+}
+
+searchButton?.addEventListener("click", (_) => {
+  const cityName = locationInput.value.trim();
+  console.log('City name : ', cityName)
+  findData(`${cityName}`);
+});
